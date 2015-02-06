4ce3566c8b73f7cb19c3e5c3dc6da82997b3750f
mntone <sd8@live.jp>
2015-02-06 20:15:50 +0900

    Implements AugustusJulianConverter.

---
+++ ExtendedDate/Converters/Solar/AugustusJulianConverter.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ExtendedDate.Converters.Solar
+{
+	public sealed class AugustusJulianConverter: JulianConverter
+	{
+		public static IDateConverter Instance
+		{
+			get { return _Instance ?? ( _Instance = new AugustusJulianConverter() ); }
+		}
+		private static IDateConverter _Instance;
+
+
+		private static readonly short[] DaysToMonth365 = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+		private static readonly short[] DaysToMonth366 = { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 };
+
+		private AugustusJulianConverter()
+		{ }
+
+		internal override short[] GetCalendar( bool isLeapYear )
+		{
+			return isLeapYear ? DaysToMonth366 : DaysToMonth365;
+		}
+	}
+}
